b7533ae4b1af93019d5ce0c6340a62699ec1f55d
James Sofra <James.Sofra@gmail.com>
2014-01-02 22:38:15 +1100

    Update project file with correct info and latest datomic dependency.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject datomic-gis "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject meridian/datomic-rtree "0.1.0-SNAPSHOT"
+  :description "An r-tree based spatial indexer for Datomic."
+  :url "http://github.com/jsofra/datomic-rtree"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.datomic/datomic-free "0.9.4324"]])
+                 [com.datomic/datomic-free "0.9.4360"]])
