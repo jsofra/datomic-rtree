41f345bb79856bad3082c2c7f122e2eda06378f1
James Sofra <James.Sofra@gmail.com>
2014-01-25 23:03:13 +1100

    Add in code into the example scratch space to demo bulk loading.

--- examples/mem_tree.clj
+++ examples/mem_tree.clj
@@ -2,7 +2,9 @@
   (:use [datomic.api :only (q db) :as d])
   (:require [clojure.java.io :as io]
             [meridian.datomic-rtree.rtree :as rtree]
-            [meridian.datomic-rtree.bbox :as bbox])
+            [meridian.datomic-rtree.bbox :as bbox]
+            [meridian.datomic-rtree.hilbert :as hilbert]
+            [meridian.datomic-rtree.bulk :as bulk])
   (:import datomic.Util))
 
 (def uri "datomic:mem://rtrees")
@@ -19,7 +21,7 @@
      (let [conn (d/connect uri)]
        (->> (read-string (slurp schema))
             (d/transact conn))
-       (d/transact conn (rtree/create-tree-tx max-children min-children))
+       @(d/transact conn (rtree/create-tree-tx max-children min-children))
        conn)))
 
 (defn install-test-data [conn]
@@ -32,17 +34,32 @@
                    [[-1.0 -5.0 50.0 50.0] "g"]
                    [[1.0 1.0 8.0 8.0] "h"]]]
     (doseq [[box entry] test-data]
-      (d/transact conn [[:rtree/insert-entry
-                         (find-tree (d/db conn))
-                         (assoc (apply bbox/extents box) :node/entry entry)]]))))
+      @(d/transact conn [[:rtree/insert-entry
+                          (find-tree (d/db conn))
+                          (assoc (apply bbox/extents box) :node/entry entry)]]))))
 
 (defn install-rand-data [conn num-entries]
-  (let [test-data (into [] (take num-entries (partition 4 (repeatedly #(rand 500)))))]
-    (time (doseq [box test-data]
-            (d/transact conn [[:rtree/insert-entry
-                               (find-tree (d/db conn))
-                               (assoc (apply bbox/extents box)
-                                 :node/entry (str (char (rand-int 200))))]])))))
+  (let [hilbert-index (hilbert/index-fn 28 [0.0 500.0])
+        test-data (into [] (take num-entries (partition 4 (repeatedly #(rand 500)))))]
+    (time (doseq [[x1 y1 x2 y2 :as box] test-data]
+            @(d/transact conn [[:rtree/insert-entry
+                                (find-tree (d/db conn))
+                                (merge (apply bbox/extents box)
+                                       {:node/entry (str (char (rand-int 200)))})]])))))
+
+
+(defn install-rand-ents [conn num-entries]
+  (let [hilbert-index (hilbert/index-fn 28 [0.0 500.0])
+        test-data (->> (take num-entries (partition 4 (repeatedly #(rand 500))))
+                       (map (fn [[x1 y1 x2 y2 :as box]]
+                              (merge (apply bbox/extents box)
+                                     {:node/entry (str (char (rand-int 200)))
+                                      :db/id (d/tempid :db.part/user)
+                                      :node/hilbert-val (hilbert-index
+                                                         [(* 0.5 (+ x1 x2))
+                                                          (* 0.5 (+ y1 y2))])}))))]
+    (time @(d/transact conn test-data))
+    :done))
 
 (defn print-tree [conn]
   (let [root (:rtree/root (find-tree (d/db conn)))]
@@ -52,7 +69,6 @@
          (walk c (str indent "---"))))
      root "")))
 
-
 (defn all-entries [db]
   (map #(d/entity db (first %))
        (d/q '[:find ?e :where [?e :node/entry]] db)))
@@ -60,11 +76,13 @@
 (defn naive-intersecting [entries search-box]
   (into [] (filter #(bbox/intersects? search-box %) entries)))
 
+(defn ent-intersects? [box ent db]
+  (bbox/intersects? box (d/entity db ent)))
+
 (def search-rules
   '[[(intersecting ?ancestor ?descendant ?search-box)
-     [?ancestor :node/children ?descendant]
-     [(datomic.api/entity $ ?descendant) ?e]
-     [(meridian.datomic-rtree.bbox/intersects? ?e ?search-box)]]
+     [(mem-tree/ent-intersects? ?search-box ?descendant $)]
+     [?ancestor :node/children ?descendant]]
     [(intersecting ?ancestor ?descendant ?search-box)
      [?ancestor :node/children ?child]
      [intersecting ?child ?descendant ?search-box]]])
@@ -76,12 +94,21 @@
     [intersecting ?root ?e ?bbox]
     [?e :node/entry]])
 
+(defn install-and-bulk-load [conn n]
+  (install-rand-ents conn n)
+  (let [ents (rtree/hilbert-ents (d/db conn))]
+    @(d/transact conn
+                 (bulk/bulk-tx ents #db/id[:db.part/user] 6 3 bbox/area))
+    :done))
+
 (comment
   (def conn (create-and-connect-db uri "resources/datomic/schema.edn" 6 3))
   (install-rand-data conn 1000)
+  (install-rand-ents conn 100)
   (def search-box (bbox/extents 0.0 0.0 10.0 10.0))
   (def root (:rtree/root (find-tree (d/db conn))))
   (time (count (naive-intersecting (all-entries (d/db conn)) search-box)))
   (time (count (rtree/intersecting root search-box)))
   (time (count (d/q intersecting-q (d/db conn) search-rules (:db/id root) search-box)))
+  (time (install-and-bulk-load conn 10000))
   )
