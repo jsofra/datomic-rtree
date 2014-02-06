483522f85913d7e833bafb5ae7e38ba5d6574cf2
James Sofra <James.Sofra@gmail.com>
2014-02-06 22:09:34 +1100

    More scratch namespace updates.

--- examples/mem_tree.clj
+++ examples/mem_tree.clj
@@ -14,34 +14,17 @@
   (->> (d/q '[:find ?e :where [?e :rtree/root]] db)
        ffirst (d/entity db)))
 
-(defn create-and-connect-db
-  ([uri schema] (create-and-connect-db uri schema 50 20))
-  ([uri schema max-children min-children]
-     (d/delete-database uri)
-     (d/create-database uri)
-     (let [conn (d/connect uri)]
-       (->> (read-string (slurp schema))
-            (d/transact conn))
-       @(d/transact conn (rtree/create-tree-tx max-children min-children))
-       conn)))
-
-(defn install-test-data [conn]
-  (let [test-data [[[0.0 0.0 10.0 10.0] "a"]
-                   [[5.0 5.0 8.0 8.0] "b"]
-                   [[0.0 0.0 20.0 20.0] "c"]
-                   [[2.0 2.0 6.0 6.0] "d"]
-                   [[-10.0 -5.0 5.0 5.0] "e"]
-                   [[-100.0 -50.0 5.0 5.0] "f"]
-                   [[-1.0 -5.0 50.0 50.0] "g"]
-                   [[1.0 1.0 8.0 8.0] "h"]]]
-    (doseq [[box entry] test-data]
-      @(d/transact conn [[:rtree/insert-entry
-                          (find-tree (d/db conn))
-                          (assoc (apply bbox/extents box) :node/entry entry)]]))))
+(defn create-and-connect-db [uri schema]
+  (d/delete-database uri)
+  (d/create-database uri)
+  (let [conn (d/connect uri)]
+    (->> (read-string (slurp schema))
+         (d/transact conn))
+    conn))
 
 (defn rand-entries []
   (let [hilbert-index (hilbert/index-fn 28 [0.0 600.0])]
-    (->> (repeatedly #(bbox/bbox (rand 550) (rand 550) (rand 50) (rand 50)))
+    (->> (repeatedly #(bbox/bbox (rand 540) (rand 540) (+ 10 (rand 50)) (+ 10 (rand 50))))
          (map (fn [box]
                 (merge box
                        {:node/entry (str (char (+ (rand-int 25) 65)))
@@ -59,6 +42,18 @@
     (time @(d/transact conn test-data))
     :done))
 
+(defn create-tree-and-install-rand-data [conn num-entries max-children min-children]
+  @(d/transact conn (rtree/create-tree-tx max-children min-children))
+  (install-rand-data conn num-entries))
+
+(defn install-and-bulk-load [conn num-entries max-children min-children]
+  (install-rand-ents conn num-entries)
+  (let [ents (rtree/hilbert-ents (d/db conn))]
+    @(d/transact conn
+                 (bulk/bulk-tx ents #db/id[:db.part/user]
+                               max-children min-children bbox/area))
+    :done))
+
 (defn print-tree [conn]
   (let [root (:rtree/root (find-tree (d/db conn)))]
     ((fn walk [n indent]
@@ -74,6 +69,7 @@
 (defn all-entries [db]
   (map #(d/entity db (first %))
        (d/q '[:find ?e :where [?e :node/entry]] db)))
+
 
 (defn naive-intersecting [entries search-box]
   (into [] (filter #(bbox/intersects? search-box %) entries)))
@@ -96,23 +92,17 @@
     [intersecting ?root ?e ?bbox]
     [?e :node/entry]])
 
-(defn install-and-bulk-load [conn n]
-  (install-rand-ents conn n)
-  (let [ents (rtree/hilbert-ents (d/db conn))]
-    @(d/transact conn
-                 (bulk/bulk-tx ents #db/id[:db.part/user] 6 3 bbox/area))
-    :done))
-
 (comment
-  (def conn (create-and-connect-db uri "resources/datomic/schema.edn" 6 3))
+  (def conn (create-and-connect-db uri "resources/datomic/schema.edn"))
   (install-rand-data conn 1000)
   (install-rand-ents conn 100)
+  (create-tree-and-install-rand-data conn 40 6 3)
   (def search-box (bbox/extents 0.0 0.0 10.0 10.0))
   (def root (:rtree/root (find-tree (d/db conn))))
   (time (count (naive-intersecting (all-entries (d/db conn)) search-box)))
   (time (count (rtree/intersecting root search-box)))
   (time (count (d/q intersecting-q (d/db conn) search-rules (:db/id root) search-box)))
-  (time (install-and-bulk-load conn 10000))
+  (time (install-and-bulk-load conn 10000 6 3))
   )
 
 ;;;;;; draw tree with quill ;;;;;;
@@ -129,15 +119,17 @@
 (defn setup-sketch []
   (frame-rate 30)
   (smooth)
-  (let [conn (create-and-connect-db uri "resources/datomic/schema.edn" 6 3)]
-    ;(install-and-bulk-load conn 50)
+  (let [conn (create-and-connect-db uri "resources/datomic/schema.edn")]
+    ;(install-and-bulk-load conn 30 6 3)
+    (create-tree-and-install-rand-data conn 1 6 3)
     (set-state! :conn conn
-                :rects (atom []))))
+                :rects (atom (all-bbox (d/db conn))))))
 
 (defn draw-sketch []
   (stroke 255)
   (fill 255)
   (rect 0 0 600 600)
+
   (doseq [r (sort-by :node/is-leaf? @(state :rects))]
     (no-fill)
     (stroke-weight 1)
